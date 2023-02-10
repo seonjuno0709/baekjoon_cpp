44ef2ee88d6c26ff47e235646da0f46718d76e6b
Seon <76555222+seonjuno0709@users.noreply.github.com>
2023-02-11 03:58:38 +0900

    add

---
+++ baekjoon_cpp/b10926.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string name;
+
+	cin >> name;
+
+	cout << name << "??!";
+
+	return 0;
+}
---
+++ baekjoon_cpp/b3003.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int k, q, l, b, n, p;
+
+	cin >> k >> q >> l >> b >> n >> p;
+
+	cout << 1 - k << " " << 1 - q << " " << 2 - l << " " << 2 - b << " " << 2 - n << " " << 8 - p;
+}
